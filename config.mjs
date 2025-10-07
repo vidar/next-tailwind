5dc8a005a7842ddb707ac36e33b2e12b5112e22f
Vidar Masson <vidar.masson@gmail.com>
2025-10-07 17:24:23 +0000

    Running locally

--- config.mjs
+++ config.mjs
@@ -6,7 +6,7 @@ import { VERSION } from "remotion/version";
  */
 export const REGION = "us-east-1";
 
-export const SITE_NAME = "my-next-app";
-export const RAM = 3009;
-export const DISK = 10240;
-export const TIMEOUT = 240;
+export const SITE_NAME = "my-video";
+export const RAM = 2048;
+export const DISK = 2048;
+export const TIMEOUT = 120;
