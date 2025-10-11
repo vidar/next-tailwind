55e619e995e0f215fb06af0cf4b063defdfc1248
Vidar Masson <vidar.masson@gmail.com>
2025-10-11 17:10:19 +0000

    auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import "../../styles/global.css";
 import { Metadata, Viewport } from "next";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Remotion and Next.js",
@@ -18,8 +26,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-background">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="bg-background">
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-blue-500 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
