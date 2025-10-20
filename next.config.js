fe360321df879c433e80579318da25f27c05b928
Vidar Masson <vidar.masson@gmail.com>
2025-10-20 15:51:04 +0000

    linting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Don't fail build on linting errors
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Don't fail build on TypeScript errors (optional - only if you want this too)
+    // ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
